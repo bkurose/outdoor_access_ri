7710525fcbe9edd1ce7a814d25800a09d0f2ffba
Ben Kurose <ben.kurose@gmail.com>
2022-11-02 14:15:21 -0400

    routes configured, sessions controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  resources :water_access_images
-  resources :water_access_comments
-  resources :water_access_ratings
-  resources :water_access_collaborators
-  resources :water_access_points
-  resources :users
+  resources :water_access_images, only: [:create, :index, :show, :destroy]
+  resources :water_access_comments, only: [:create, :index, :show, :destroy, :update]
+  resources :water_access_ratings, only: [:create, :index, :show, :update]
+  resources :water_access_collaborators, only: [:create, :index, :show, :destroy, :update]
+  resources :water_access_points, only: [:create, :index, :show, :destroy, :update]
+  resources :users, only: [:create, :show, :destroy, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  #test route
   get '/hello', to: 'application#hello_world'
+
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
