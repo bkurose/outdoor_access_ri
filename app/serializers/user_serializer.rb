adb93e0d68f66e6f87b61b051607e4576be0e12c
Ben Kurose <ben.kurose@gmail.com>
2022-11-02 14:44:55 -0400

    basic serializers added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :password_digest, :username, :email
+
+  has_many :water_access_comments
+  has_many :water_access_points
+  has_many :water_access_images
+  has_many :water_access_collaborators
 end
--- app/serializers/water_access_point_serializer.rb
+++ app/serializers/water_access_point_serializer.rb
@@ -1,3 +1,8 @@
 class WaterAccessPointSerializer < ActiveModel::Serializer
   attributes :id, :name, :lat, :long, :type, :description, :dogs, :town, :trail_map, :handicap_accessible, :details, :traffic
+
+  has_many :water_access_comments
+  has_many :water_access_images
+  has_many :water_access_collaborators
+  has_many :water_access_ratings
 end
