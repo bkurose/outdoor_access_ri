2a4135f2ae011afb0f4fb3cc8b5dc712cffd35db
Ben Kurose <ben.kurose@gmail.com>
2022-11-09 18:53:55 -0500

    Rights updated

--- client/src/Rights.js
+++ client/src/Rights.js
@@ -5,6 +5,11 @@ function Rights (){
         <div>
             <NavBar />
             <h1>RIghts</h1>
+            <p>Article 1, Section 17 of the Rhode Island Constitution:
+            <br/>“The people shall continue to enjoy and freely exercise all the rights of fishery, and the privileges of the shore, to which they have been heretofore entitled under the charter and usages of this state, including but not limited to fishing from the shore, the gathering of seaweed, leaving the shore to swim in the sea and passage along the shore”
+            <br/>
+            <br/>Article 1, Section 16 of the Rhode Island Constitution:
+            <br/>“…furtherance of the protection of the rights of the people to enjoy and freely exercise the rights of fishery and the privileges of the shore, as those rights and duties are set forth in section 17, shall be an exercise of the police powers of the state, shall be liberally construed, and shall not be deemed to be a public use of private property.”</p>
         </div>
     )
 }
