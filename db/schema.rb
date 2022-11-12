82c32c833b3818cf5addc06339754c2f4c1e0f1f
Ben Kurose <ben.kurose@gmail.com>
2022-11-12 14:00:24 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_170611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "water_access_collaborators", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "water_access_point_id"
+    t.boolean "full_edit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "water_access_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "water_access_point_id"
+    t.string "comment_title"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "water_access_images", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "water_access_point_id"
+    t.string "file_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "water_access_points", force: :cascade do |t|
+    t.string "name"
+    t.float "lat"
+    t.float "long"
+    t.string "access_type"
+    t.string "description"
+    t.boolean "dogs"
+    t.string "town"
+    t.string "trail_map"
+    t.string "handicap_accessible"
+    t.string "details"
+    t.string "traffic"
+    t.string "parking"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "water_access_ratings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "water_access_point_id"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
